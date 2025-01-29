7e8c73a9685323ef3eec0c0021963058375c4687
meerimvz <meerimzulpukarova5@gmail.com>
2025-01-29 14:40:43 +0600

    Create user.py

---
+++ user.py
@@ -0,0 +1,5 @@
+user = {}
+user["name"] = input("What is the user's name? ")
+user["age"] = input("What is the user's age? ")
+user["birth_country"] = input("What is the user's country of birth? ")
+user["known_for"] = input("What is the user known for? ")
