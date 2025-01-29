6e23268020b1f3e4c0819b48dd09c2ce4e27b36a
meerimvz <meerimzulpukarova5@gmail.com>
2025-01-29 14:39:14 +0600

    Create myfamily.py

---
+++ myfamily.py
@@ -0,0 +1,13 @@
+myfamily = ("father", "mother", "sister", "brother", "sister")
+print(myfamily)
+
+#task1
+print(type(myfamily))
+
+#task2
+print(myfamily[2])
+print(myfamily[4])
+
+#in task 3 we cannot add a new item by append() because tuples are immutable
+#in task 4 we also cannot use pop() method 
+
