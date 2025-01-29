0eeafbeabb1a932d1a2353dc39404b8c3d0d3532
meerimvz <meerimzulpukarova5@gmail.com>
2025-01-29 14:39:54 +0600

    Create laptop.py

---
+++ laptop.py
@@ -0,0 +1,13 @@
+laptop = {"brand":"dell", "model":"alienware", "year":2010 }
+
+#task1
+brand_value = list(laptop.keys())[0]
+print(laptop[brand_value])
+
+#task2
+laptop["home"] = True
+print(laptop)
+
+#task3
+laptop["year"] = 2019
+print(laptop)
