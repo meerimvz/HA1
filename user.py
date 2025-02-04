be13778bc6e5c4c6d13488d5d5e12d08c00e1b68
meerimvz <meerimzulpukarova5@gmail.com>
2025-02-04 11:00:13 +0600

    Update user.py

--- user.py
+++ user.py
@@ -1,5 +1,5 @@
 user = {}
 user["name"] = input("What is the user's name? ")
-user["age"] = input("What is the user's age? ")
+user["age"] = int(input("What is the user's age? "))
 user["birth_country"] = input("What is the user's country of birth? ")
 user["known_for"] = input("What is the user known for? ")
